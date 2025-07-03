811680f9df03d8f72ede1ccead9162a5f6cb7cfc
CaRTipe <blairmbugua868@gmail.com>
2025-07-03 09:52:04 +0300

    Add .gitignore, package-lock.json, and update package.json for dependencies

--- index.js
+++ index.js
@@ -1,67 +1,42 @@
-// This code defines three functions that return promises for walking, feeding, and playing with a dog.
-function walkDog() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const walked = true;
-    if(walked) {
-      resolve('Dog has been walked 🐕');
-    }
-    else {
-      reject('Dog has not been walked');
-    }
-  }, 1500)});
-}
-function feedDog() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const fed = true;
-      if(fed) {
-        resolve('Dog has been fed 🍴');
-      }
-      else {
-        reject('Dog has not been fed');
-      }
-    }, 1500);
-  });
-}
-function playWithDog() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const played = true;
-    if(played) {
-      resolve('Dog has been played with 🎾');
-    }
-    else {
-      reject('Dog has not been played with');
-    }
-  }, 1500);
-})}
-;
+import * as CanvasJS from "@canvasjs/charts";
 
+// Bar Chart
+const barChart = new CanvasJS.Chart("barChart", {
+    animationEnabled: true,
+    theme: "light2",
+    title: {
+        text: "Monthly Sales"
+    },
+    axisY: {
+        title: "Revenue (USD)"
+    },
+    data: [{
+        type: "column",
+        dataPoints: [
+            { label: "Jan", y: 3000 },
+            { label: "Feb", y: 5000 },
+            { label: "Mar", y: 4000 }
+        ]
+    }]
+});
+barChart.render();
 
-async function takeCareOfDog() {
-  try {
-    const walkResult = await walkDog();
-    console.log(walkResult);
-    
-    const feedResult = await feedDog();
-    console.log(feedResult);
-    
-    const playResult = await playWithDog();
-    console.log(playResult);
-    
-  } catch (error) {
-    console.error(error);
-  }
-}
-takeCareOfDog();
-
-
-import {PI, getCircumference, getArea, getVolume} from './mathUtil.js';
-
-console.log(`Value of PI: ${PI}`);
-console.log(`Circumference of circle with radius 5: ${getCircumference(5)}`);
-console.log(`Area of circle with radius 5: ${getArea(5)}`);   
-console.log(`Volume of sphere with radius 5: ${getVolume(5)}`);
-
-
+// Pie Chart
+const pieChart = new CanvasJS.Chart("pieChart", {
+    animationEnabled: true,
+    title: {
+        text: "User Distribution"
+    },
+    data: [{
+        type: "pie",
+        startAngle: 240,
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+            { y: 45, label: "Chrome" },
+            { y: 25, label: "Safari" },
+            { y: 15, label: "Firefox" },
+            { y: 15, label: "Others" }
+        ]
+    }]
+});
+pieChart.render();
